c5eeb0358de8f9a153ced531272c8ad48da0ae65
Erik Sternheden <erik.sternheden@gmail.com>
2020-05-03 20:58:39 +0200

    Added db file

---
+++ db/paddle.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 03 maj 2020 kl 20:56
+-- Serverversion: 10.1.30-MariaDB
+-- PHP-version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `racer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `highscore`
+--
+
+CREATE TABLE `highscore` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `highscore` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(250) NOT NULL,
+  `name` varchar(124) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `email` varchar(124) NOT NULL,
+  `activate_code` int(124) NOT NULL,
+  `create_date` date NOT NULL,
+  `uid` varchar(124) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `highscore`
+--
+ALTER TABLE `highscore`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `highscore`
+--
+ALTER TABLE `highscore`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT för tabell `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- settings.php
+++ settings.php
@@ -5,7 +5,7 @@
 define('APP_PATH', dirname($_SERVER['PHP_SELF']));
 
 define('DB_DNS', 'localhost');
-define('DB_NAME', 'racer');
+define('DB_NAME', 'paddle');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
